08b50e0c2fee6a712cf85faa3e3a3fee32c19a8a
Moaz <67188321+MeYo0o@users.noreply.github.com>
2022-04-14 17:33:28 +0200

    Add constants

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //Colors
+  static const primaryColor = Color(0xFF0FA59A);
+  static const secondaryColor = Colors.yellow;
+
+  static const startLinearGradientColor = Color(0xFF245550);
+  static const lastLinearGradientColor = Color(0xFF13A69A);
+}
---
+++ lib/core/constants/design_screen_size.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const Size kDesignScreenSize = Size(392.72727272727275, 759.2727272727273);
